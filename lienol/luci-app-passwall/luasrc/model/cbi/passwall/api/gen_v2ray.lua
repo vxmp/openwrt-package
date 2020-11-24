ec006f7f8a27a3f347133d2e8921190f45744eaf
vxmp <33788416+vxmp@users.noreply.github.com>
2020-11-25 00:12:33 +0800

    Update gen_v2ray.lua

--- lienol/luci-app-passwall/luasrc/model/cbi/passwall/api/gen_v2ray.lua
+++ lienol/luci-app-passwall/luasrc/model/cbi/passwall/api/gen_v2ray.lua
@@ -26,7 +26,6 @@ end
 
 function gen_outbound(node, tag, relay_port)
     local result = nil
-    local ua = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36"
     if node then
         local node_id = node[".name"]
         if tag == nil then
