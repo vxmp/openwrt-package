e486485ccdb07d886363b38a77fe74a7a65defe7
vxmp <33788416+vxmp@users.noreply.github.com>
2020-11-24 23:34:54 +0800

    Update gen_v2ray.lua

--- lienol/luci-app-passwall/luasrc/model/cbi/passwall/api/gen_v2ray.lua
+++ lienol/luci-app-passwall/luasrc/model/cbi/passwall/api/gen_v2ray.lua
@@ -105,11 +105,12 @@ function gen_outbound(node, tag, relay_port)
                 } or nil,
                 wsSettings = (node.transport == "ws") and {
                     path = node.ws_path or "",
-                    headers = (node.ws_host ~= nil) and
-                        {
+                    headers = (node.ws_host ~= nil) and {
+                      {
                           Host = node.ws_host or nil,
                           User-Agent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36"
-                        }
+                      }
+                    }
                 } or nil,
                 httpSettings = (node.transport == "h2") and
                     {path = node.h2_path, host = node.h2_host} or
